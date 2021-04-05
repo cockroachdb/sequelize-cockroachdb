c1efa2ac50a9a2e6ff6d476a53cefa49f4be7e8e
Douglas Marques <douglas.marques@codeminer42.com>
2021-04-05 17:48:07 -0300

    Add patch for findOrCreate

--- source/index.js
+++ source/index.js
@@ -136,6 +136,112 @@ Model.findByPk = async function findByPk(param, options) {
   return await this.findOne(options);
 };
 
+// [6] Skips searching for err.fields
+// CRDB does not work with "details" at error level, so Sequelize does not generate this error properly.
+// Copied from: https://github.com/sequelize/sequelize/blob/29901187d9560e7d51ae1f9b5f411cf0c5d8994a/lib/model.js#L2270
+Model.findOrCreate = async function findOrCreate(options) {
+  const _ = require('lodash');
+  const Utils = require('sequelize/lib/utils.js');
+  const { logger } = require('sequelize/lib/utils/logger');
+  const sequelizeErrors = require('sequelize/lib/errors');
+
+  if (!options || !options.where || arguments.length > 1) {
+    throw new Error(
+      'Missing where attribute in the options parameter passed to findOrCreate. ' +
+      'Please note that the API has changed, and is now options only (an object with where, defaults keys, transaction etc.)'
+    );
+  }
+
+  options = { ...options };
+
+  if (options.defaults) {
+    const defaults = Object.keys(options.defaults);
+    const unknownDefaults = defaults.filter(name => !this.rawAttributes[name]);
+
+    if (unknownDefaults.length) {
+      logger.warn(`Unknown attributes (${unknownDefaults}) passed to defaults option of findOrCreate`);
+    }
+  }
+
+  if (options.transaction === undefined && this.sequelize.constructor._cls) {
+    const t = this.sequelize.constructor._cls.get('transaction');
+    if (t) {
+      options.transaction = t;
+    }
+  }
+
+  const internalTransaction = !options.transaction;
+  let values;
+  let transaction;
+
+  try {
+    const t = await this.sequelize.transaction(options);
+    transaction = t;
+    options.transaction = t;
+
+    const found = await this.findOne(Utils.defaults({ transaction }, options));
+    if (found !== null) {
+      return [found, false];
+    }
+
+    values = { ...options.defaults };
+    if (_.isPlainObject(options.where)) {
+      values = Utils.defaults(values, options.where);
+    }
+
+    options.exception = true;
+    options.returning = true;
+
+    try {
+      const created = await this.create(values, options);
+      if (created.get(this.primaryKeyAttribute, { raw: true }) === null) {
+        // If the query returned an empty result for the primary key, we know that this was actually a unique constraint violation
+        throw new sequelizeErrors.UniqueConstraintError();
+      }
+
+      return [created, true];
+    } catch (err) {
+      if (!(err instanceof sequelizeErrors.UniqueConstraintError)) throw err;
+      const flattenedWhere = Utils.flattenObjectDeep(options.where);
+      const flattenedWhereKeys = Object.keys(flattenedWhere).map(name => _.last(name.split('.')));
+      const whereFields = flattenedWhereKeys.map(name => _.get(this.rawAttributes, `${name}.field`, name));
+      const defaultFields = options.defaults && Object.keys(options.defaults)
+        .filter(name => this.rawAttributes[name])
+        .map(name => this.rawAttributes[name].field || name);
+
+      const errFieldKeys = Object.keys(err.fields || {});
+      const errFieldsWhereIntersects = Utils.intersects(errFieldKeys, whereFields);
+      if (defaultFields && !errFieldsWhereIntersects && Utils.intersects(errFieldKeys, defaultFields)) {
+        throw err;
+      }
+
+      if (errFieldsWhereIntersects) {
+        _.each(err.fields, (value, key) => {
+          const name = this.fieldRawAttributesMap[key].fieldName;
+          if (value.toString() !== options.where[name].toString()) {
+            throw new Error(`${this.name}#findOrCreate: value used for ${name} was not equal for both the find and the create calls, '${options.where[name]}' vs '${value}'`);
+          }
+        });
+      }
+
+      // Someone must have created a matching instance inside the same transaction since we last did a find. Let's find it!
+      const otherCreated = await this.findOne(Utils.defaults({
+        transaction: internalTransaction ? null : transaction
+      }, options));
+
+      // Sanity check, ideally we caught this at the defaultFeilds/err.fields check
+      // But if we didn't and instance is null, we will throw
+      if (otherCreated === null) throw err;
+
+      return [otherCreated, false];
+    }
+  } finally {
+    if (internalTransaction && transaction) {
+      await transaction.commit();
+    }
+  }
+}
+
 //// Done!
 
 Sequelize.supportsCockroachDB = true;

1aca052d3595666651c8bae3d74491945368729e
Douglas Marques <douglas.bmarques@hotmail.com>
2021-04-28 10:32:39 -0300

    Lint

--- tests/model_attributes_field_test.js
+++ tests/model_attributes_field_test.js
@@ -6,76 +6,96 @@ const sinon = require('sinon'),
   { DataTypes } = require('../source');
 
 describe('Model', () => {
-  before(function() {
+  before(function () {
     this.clock = sinon.useFakeTimers();
   });
 
-  after(function() {
+  after(function () {
     this.clock.restore();
   });
 
   describe('attributes', () => {
     describe('field', () => {
-      beforeEach(async function() {
+      beforeEach(async function () {
         const queryInterface = this.sequelize.getQueryInterface();
 
-        this.User = this.sequelize.define('user', {
-          id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-            field: 'userId'
+        this.User = this.sequelize.define(
+          'user',
+          {
+            id: {
+              type: DataTypes.INTEGER,
+              allowNull: false,
+              primaryKey: true,
+              autoIncrement: true,
+              field: 'userId'
+            },
+            name: {
+              type: DataTypes.STRING,
+              field: 'full_name'
+            },
+            taskCount: {
+              type: DataTypes.INTEGER,
+              field: 'task_count',
+              defaultValue: 0,
+              allowNull: false
+            }
           },
-          name: {
-            type: DataTypes.STRING,
-            field: 'full_name'
-          },
-          taskCount: {
-            type: DataTypes.INTEGER,
-            field: 'task_count',
-            defaultValue: 0,
-            allowNull: false
+          {
+            tableName: 'users',
+            timestamps: false
           }
-        }, {
-          tableName: 'users',
-          timestamps: false
-        });
+        );
 
-        this.Task = this.sequelize.define('task', {
-          id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-            field: 'taskId'
+        this.Task = this.sequelize.define(
+          'task',
+          {
+            id: {
+              type: DataTypes.INTEGER,
+              allowNull: false,
+              primaryKey: true,
+              autoIncrement: true,
+              field: 'taskId'
+            },
+            title: {
+              type: DataTypes.STRING,
+              field: 'name'
+            }
           },
-          title: {
-            type: DataTypes.STRING,
-            field: 'name'
+          {
+            tableName: 'tasks',
+            timestamps: false
           }
-        }, {
-          tableName: 'tasks',
-          timestamps: false
-        });
+        );
 
-        this.Comment = this.sequelize.define('comment', {
-          id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-            field: 'commentId'
+        this.Comment = this.sequelize.define(
+          'comment',
+          {
+            id: {
+              type: DataTypes.INTEGER,
+              allowNull: false,
+              primaryKey: true,
+              autoIncrement: true,
+              field: 'commentId'
+            },
+            text: { type: DataTypes.STRING, field: 'comment_text' },
+            notes: { type: DataTypes.STRING, field: 'notes' },
+            likes: { type: DataTypes.INTEGER, field: 'like_count' },
+            createdAt: {
+              type: DataTypes.DATE,
+              field: 'created_at',
+              allowNull: false
+            },
+            updatedAt: {
+              type: DataTypes.DATE,
+              field: 'updated_at',
+              allowNull: false
+            }
           },
-          text: { type: DataTypes.STRING, field: 'comment_text' },
-          notes: { type: DataTypes.STRING, field: 'notes' },
-          likes: { type: DataTypes.INTEGER, field: 'like_count' },
-          createdAt: { type: DataTypes.DATE, field: 'created_at', allowNull: false },
-          updatedAt: { type: DataTypes.DATE, field: 'updated_at', allowNull: false }
-        }, {
-          tableName: 'comments',
-          timestamps: true
-        });
+          {
+            tableName: 'comments',
+            timestamps: true
+          }
+        );
 
         this.User.hasMany(this.Task, {
           foreignKey: 'user_id'
@@ -166,7 +186,7 @@ describe('Model', () => {
       });
 
       describe('field and attribute name is the same', () => {
-        beforeEach(async function() {
+        beforeEach(async function () {
           await this.Comment.bulkCreate([
             // Added ids to Comment
             { id: 1, notes: 'Number one' },
@@ -174,7 +194,7 @@ describe('Model', () => {
           ]);
         });
         // Edited beforeEach of this test since it looks for Pk 1, and it's not guaranteed id will be 1.
-        it('reload should work', async function() {
+        it('reload should work', async function () {
           const comment = await this.Comment.findByPk(1);
           await comment.reload();
         });

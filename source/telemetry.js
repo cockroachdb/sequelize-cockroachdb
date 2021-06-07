90978843a069078e90172379bf560086b09f9092
richardjcai <caioftherichard@gmail.com>
2021-06-07 18:50:01 -0400

    Pull Cockroach Version version into helper

--- source/telemetry.js
+++ source/telemetry.js
@@ -9,16 +9,8 @@ Sequelize.addHook('afterInit', async (connection) => {
         if (telemetryDisabled) {
             return 
         }
-        const versionRow = await connection.query("SELECT version() AS version", { type: QueryTypes.SELECT });
-        version = versionRow[0]["version"]
-
-        // This regex pattern should match any version 21.1 and greater.
-        // Also assumes that the naming scheme will always follow v[XX].[Y].[ZZZZ]
-        // Where XX is the last two digits of the year, Y is the subrelease (currently is always 1/2) and
-        // ZZZZ is the point release version.
-        // This has to be updated if the version format changes or a new version of CockroachDB is released in the year 2100.
-        var re = new RegExp('v([2-9][1-9]|[3-9][0-9]).[1-2].[0-9]*')
-        if (!version.match(re)) {
+ 
+        if (!CockroachVersionGreaterThan21_1(connection)) {
             return
         }
         var sequelizeVersion = version_helper.GetSequelizeVersion()
--- source/version_helper.js
+++ source/version_helper.js
@@ -1,5 +1,6 @@
 const semver = require('semver');
 const { version, release } = require('sequelize/package.json');
+const { QueryTypes } = require('sequelize');
 
 module.exports = {
   GetSequelizeVersion: function() {
@@ -10,5 +11,17 @@ module.exports = {
       // in that case we fallback to a branch version
       return semver.coerce(version === '0.0.0-development' ? branchVersion : version);
   },
+  CockroachVersionGreaterThan21_1: async function(connection) {
+    const versionRow = await connection.query("SELECT version() AS version", { type: QueryTypes.SELECT });
+    const cockroachDBVersion = versionRow[0]["version"]
+
+    // This regex pattern should match any version 21.1 and greater.
+    // Also assumes that the naming scheme will always follow v[XX].[Y].[ZZZZ]
+    // Where XX is the last two digits of the year, Y is the subrelease (currently is always 1/2) and
+    // ZZZZ is the point release version.
+    // This has to be updated if the version format changes or a new version of CockroachDB is released in the year 2100.
+    var re = new RegExp('v([2-9][1-9]|[3-9][0-9]).[1-2].[0-9]*')
+    return cockroachDBVersion.match(re)
+  }
 };
 

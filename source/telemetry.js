2bee905c2667462c9e574875f7f571fe1f97c00c
richardjcai <caioftherichard@gmail.com>
2021-07-14 11:00:03 -0400

    Call crdb_internal.increment_feature_counter with the sequelize-cockroachdb adapter version.
    The adapter's telemetry has the format "Sequelize Adapter 6.0.0"

--- source/telemetry.js
+++ source/telemetry.js
@@ -19,6 +19,10 @@ Sequelize.addHook('afterInit', async (connection) => {
         var sequelizeVersion = version_helper.GetSequelizeVersion()
         await connection.query(`SELECT crdb_internal.increment_feature_counter(concat('Sequelize ', :SequelizeVersion))`,  
         { replacements: { SequelizeVersion: sequelizeVersion.version  }, type: QueryTypes.SELECT })
+
+        var adapterVersion = version_helper.GetAdapterVersion()
+        await connection.query(`SELECT crdb_internal.increment_feature_counter(concat('sequelize-cockroachdb ', :AdapterVersion))`,  
+        { replacements: { AdapterVersion: adapterVersion.version }, type: QueryTypes.SELECT })
     } catch (error) {
         console.info("Could not record telemetry.\n" + error)
     }

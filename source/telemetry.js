e8fa0351a0c0df2a5d9b059759f1fcceabbade88
Moh Achun Armando <mohamad.armando@kumparan.com>
2021-12-17 08:06:57 +0700

    fix promise condition

--- source/telemetry.js
+++ source/telemetry.js
@@ -8,22 +8,22 @@ Sequelize.addHook('afterInit', async (connection) => {
         if (connection.options.dialectOptions) {
             var telemetryDisabled = connection.options.dialectOptions.cockroachdbTelemetryDisabled
             if (telemetryDisabled) {
-                return 
+                return
             }
         }
- 
+
         // crdb_internal.increment_feature_counter is only available on 21.1 and above.
-        if (!version_helper.IsCockroachVersion21_1Plus(connection)) {
+        if (!(await version_helper.IsCockroachVersion21_1Plus(connection))) {
             return
         }
         var sequelizeVersion = version_helper.GetSequelizeVersion()
         var sequelizeVersionSeries = version_helper.GetVersionSeries(sequelizeVersion.version)
         var sequelizeVersionStr = (sequelizeVersionSeries===null)?sequelizeVersion:sequelizeVersionSeries
-        await connection.query(`SELECT crdb_internal.increment_feature_counter(concat('Sequelize ', :SequelizeVersionString))`,  
+        await connection.query(`SELECT crdb_internal.increment_feature_counter(concat('Sequelize ', :SequelizeVersionString))`,
         { replacements: { SequelizeVersionString: sequelizeVersionStr }, type: QueryTypes.SELECT })
 
         var adapterVersion = version_helper.GetAdapterVersion()
-        await connection.query(`SELECT crdb_internal.increment_feature_counter(concat('sequelize-cockroachdb ', :AdapterVersion))`,  
+        await connection.query(`SELECT crdb_internal.increment_feature_counter(concat('sequelize-cockroachdb ', :AdapterVersion))`,
         { replacements: { AdapterVersion: adapterVersion.version }, type: QueryTypes.SELECT })
     } catch (error) {
         console.info("Could not record telemetry.\n" + error)
